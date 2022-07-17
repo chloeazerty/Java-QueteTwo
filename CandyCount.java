d7281092d0bb19a38e16412b2d9c46ce79e34d92
chloe azerty <chloe.rotaru@yahoo.com>
2022-07-17 15:40:29 +0200

    update

--- CandyCount.java
+++ CandyCount.java
@@ -6,13 +6,12 @@ class CandyCount {
         int candies = 0;
 
         if (money > 0 && price > 0) {
-            while (money - price >= 0) ;
+            while (money - price >= 0);
             {
                 candies = candies + 1;
                 money = money - price;
             }
-            System.out.println(candies);
-
         }
+        System.out.println(candies);
     }
 }
